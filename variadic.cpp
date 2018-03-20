bbfe2db662a86c7c0352db586451106016d5f0da
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-03-20 11:32:25 +0100

    semana santa

--- variadic.cpp
+++ variadic.cpp
@@ -1,79 +1,101 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+#define SUM 1
+#define RES 2
+#define MUL 3
+#define DIV 4
+#define MAT 5
+#define RAI 6
+#define POT 7
 
-#define SUM 0
-#define RES 1
-#define MUL 2
-#define DIV 3
-
-
+#define N   3
 int main(){
     unsigned operacion;
-	double op1;
-	double op2;
-        double suma;
+    double op1;
+    double op2;
+    int a[N][N];
+    double s;
+    double resultado;  
+  
 
 
-	printf(
-            "Elige operacion. \n"
-            "\n"
-            "\t0. SUMA \n"
-            "\t1. RESTA \n"
-            "\t2. MULTIPLICACION \n"
-            "\t3. DIVISION. \n"
-            "\tOperacion :"
-            "\n"
-              );
-	scanf(" %lf", &suma);
 
-	if (suma == 0) 
-	{
-		printf("operando1:");
-		scanf(" %lf", &op1);
-		printf("operando2:");
-		scanf(" %lf", &op2);
-
-		printf(" %.2lf + %.2lf =%.2lf\n", op1, op2, op1 + op2);
-
-	}
-
-	else if (suma == 1)
-	{
-		printf("operando1:");
-		scanf(" %lf", &op1);
-		printf("operando2:");
-		scanf(" %lf", &op2);
+    system("toilet -fpagga  Calculator");
 
 
-		printf(" %.2lf - %.2lf =%.2lf\n", op1, op2, op1 - op2);
-	}
-
-	else if(suma == 2)
-{
-	printf("entonces multiplicar \n");
-	printf("operando1:");
-	scanf(" %lf", &op1);
-	printf("operando2:");
-	scanf(" %lf", &op2);
+    printf("QUE OPERACION QUIERES:");
+    scanf(" %i", &operacion);
 
 
-	printf(" %.2lf * %.2lf =%.2lf\n", op1, op2, op1 * op2);
 
+    switch(operacion){
+        case SUM:
+            printf("Suma:\n");
+            printf("operando1:");
+            printf("entonces sumar\n");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf + %.2lf =%.2lf\n", op1, op2, op1 + op2);
+            break;
+        case RES:
+            printf("Resta:\n");
+            printf(" entonces restar\n");
+            printf("operando1:");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf - %.2lf =%.2lf\n", op1, op2, op1 - op2);
+            break;
+        case MUL:
+            printf("Multiplicacion: \n");
+            printf("entonces multiplicar \n");
+            printf("operando1:");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf * %.2lf =%.2lf\n", op1, op2, op1 * op2);
+            break;
+        case DIV:
+            printf("Division:\n");
+            printf("entonces dividr \n");
+            printf("operando1:");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf / %.2lf =%.2lf\n", op1, op2, op1 / op2);
+            break;
+            /* case MAT:
+               printf("Matriz:\n");
+               for(int f=0; f<N; f++)
+               for(int c=0; c<N; c++){
+               printf("mete numeros:");
+               scanf(" %i", &a[N][N]);
+
+
+               }
+               printf(" el resultdo es %.6lf \n", resultado);
+               */
+        case RAI:
+            printf("Raiz Cuadrada:\n");
+            printf("Entonces hacemos la raiz\n");
+            printf("Numero:");
+            scanf("%lf", &s);
+            s=sqrt(s);
+            printf("El Resultado de la Raiz es: %lf\n", s);
+            system("PAUSE");
+        case POT:
+            printf("Potencia\n");
+            printf("Hace,os la potencia\n");
+          
+            
+      
+    default:
+    printf("del 1 al 6 huevon \n");
+    break;
 }
-	else
-	{
-		printf("entonces dividr \n");
-		printf("operando1:");
-		scanf(" %lf", &op1);
-		printf("operando2:");
-		scanf(" %lf", &op2);
 
-
-		printf(" %.2lf / %.2lf =%.2lf\n", op1, op2, op1 / op2);
-
-
-	}
-
-
-	return EXIT_SUCCESS;
+return EXIT_SUCCESS;
 }
+
