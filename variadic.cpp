9e2373edbdec36f6b347ff4e90232d2a7c79cfac
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-03-21 13:42:27 +0100

    con

--- variadic.cpp
+++ variadic.cpp
@@ -1,18 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+
+#define SIM 1
+#define CON 2
+
+
 #define SUM 1
 #define RES 2
 #define MUL 3
 #define DIV 4
+
 #define MAT 5
 #define RAI 6
 #define LOG 7
 #define POR 8
+
+
+
+
+#define MIL  1
+#define CEN  2
+#define DEC  3
+
 #define N   3
-
-
-const char *elegir[] ={
+const char *elegir0[] = {
+    "SIMPLES",
+    "CONVERSION",
+    NULL
+};
+const char *elegir1[] ={
     "SUMA",
     "RESTA",
     "MULTIPLICACION",
@@ -23,9 +40,17 @@ const char *elegir[] ={
     "PORCENTAJE",
     NULL
 };
+const char *elegir2[]={
+    "Milimetros",
+    "Centrimetros",
+    "Decimetros",
+    NULL   
+};
 
 int main(){
+    unsigned operaciones;
     unsigned operacion;
+    unsigned conversion;
     double op1;
     double op2;
     int a[N][N];
@@ -36,96 +61,113 @@ int main(){
 
 
     system("toilet -fpagga  Calculator");
-
+    for(int o=0; o<2; o++)
+        printf("%i. %s.\n", o+1, elegir0[o]);
+    scanf("%i", &operaciones);
 
 
 
     for(int i=0; i<8; i++)
-        printf("%i. %s.\n", i+1, elegir[i]);
-    printf("QUE OPERACION QUIERES:");
-
+        printf("%i. %s.\n", i+1, elegir1[i]);
+    printf("QUE OPERACION SIMPLE QUIERES:");
     scanf(" %i", &operacion);
+    for(int s=0; s<3; s++)
+        printf("%i. %s.\n", s+1, elegir2[s]);
+    printf("que conversion a metros quieres:");
+    scanf("%i", &conversion);
+    switch(operaciones){
+        case SIM:
+            printf("operaciones simples:");
+            break;
+        case CON:
+            printf("CONVERSIONES A METROS:");
+            break;
+        default:
+            printf("del 1 o 2\n");
 
-    
-        switch(operacion){
-            case SUM:
-                printf("Suma:\n");
-                printf("operando1:");
-                printf("entonces sumar\n");
-                scanf(" %lf", &op1);
-                printf("operando2:");
-                scanf(" %lf", &op2);
-                printf(" %.2lf + %.2lf =%.2lf\n", op1, op2, op1 + op2);
-                break;
-            case RES:
-                printf("Resta:\n");
-                printf(" entonces restar\n");
-                printf("operando1:");
-                scanf(" %lf", &op1);
-                printf("operando2:");
-                scanf(" %lf", &op2);
-                printf(" %.2lf - %.2lf =%.2lf\n", op1, op2, op1 - op2);
-                break;
-            case MUL:
-                printf("Multiplicacion: \n");
-                printf("entonces multiplicar \n");
-                printf("operando1:");
-                scanf(" %lf", &op1);
-                printf("operando2:");
-                scanf(" %lf", &op2);
-                printf(" %.2lf * %.2lf =%.2lf\n", op1, op2, op1 * op2);
-                break;
-            case DIV:
-                printf("Division:\n");
-                printf("entonces dividr \n");
-                printf("operando1:");
-                scanf(" %lf", &op1);
-                printf("operando2:");
-                scanf(" %lf", &op2);
-                printf(" %.2lf / %.2lf =%.2lf\n", op1, op2, op1 / op2);
-                break;
-                /* case MAT:
-                   printf("Matriz:\n");
-                   for(int f=0; f<N; f++)
-                   for(int c=0; c<N; c++){
-                   printf("mete numeros:");
-                   scanf(" %i", &a[N][N]);
+    }
+
+    switch(operacion){
+        case SUM:
+            printf("Suma:\n");
+            printf("operando1:");
+            printf("entonces sumar\n");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf + %.2lf =%.2lf\n", op1, op2, op1 + op2);
+            break;
+        case RES:
+            printf("Resta:\n");
+            printf(" entonces restar\n");
+            printf("operando1:");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf - %.2lf =%.2lf\n", op1, op2, op1 - op2);
+            break;
+        case MUL:
+            printf("Multiplicacion: \n");
+            printf("entonces multiplicar \n");
+            printf("operando1:");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf * %.2lf =%.2lf\n", op1, op2, op1 * op2);
+            break;
+        case DIV:
+            printf("Division:\n");
+            printf("entonces dividr \n");
+            printf("operando1:");
+            scanf(" %lf", &op1);
+            printf("operando2:");
+            scanf(" %lf", &op2);
+            printf(" %.2lf / %.2lf =%.2lf\n", op1, op2, op1 / op2);
+            break;
+            /* case MAT:
+               printf("Matriz:\n");
+               for(int f=0; f<N; f++)
+               for(int c=0; c<N; c++){
+               printf("mete numeros:");
+               scanf(" %i", &a[N][N]);
 
 
-                   }
-                   printf(" el resultdo es %.6lf \n", resultado);
-                   */
-            case RAI:
-                printf("Raiz Cuadrada:\n");
-                printf("Entonces hacemos la raiz\n");
-                printf("Numero:");
-                scanf("%lf", &s);
-                s=sqrt(s);
-                printf("El Resultado de la Raiz es: %lf\n", s);
-                break;
+               }
+               printf(" el resultdo es %.6lf \n", resultado);
+               */
+        case RAI:
+            printf("Raiz Cuadrada:\n");
+            printf("Entonces hacemos la raiz\n");
+            printf("Numero:");
+            scanf("%lf", &s);
+            s=sqrt(s);
+            printf("El Resultado de la Raiz es: %lf\n", s);
+            break;
 
-            case LOG:
-                printf("Logaritmo\n");
-                printf("Hacemos el logaritmo\n");
-                printf("mete el  numero:\n");
-                scanf("%lf", &s);
-                s = log10(s);
-                printf("El logaritmo de %lf\n", s);
-                break;
-            case POR:
-                printf("Porcentaje:\n");
-                printf("Hacamos el porcentaje\n");
-                printf("dime el numero:");
-                scanf("%lf", &s);
-                printf("dime el tanto por ciento:");
-                scanf("%lf", &p);
-               s = s* p/100;
-               printf("el porcentaje es %.2lf\n", s);
-               break;
-            default:
-                printf("del 1 al 6 huevon \n");
-        }
-    
+        case LOG:
+            printf("Logaritmo\n");
+            printf("Hacemos el logaritmo\n");
+            printf("mete el  numero:\n");
+            scanf("%lf", &s);
+            s = log10(s);
+            printf("El logaritmo de %lf\n", s);
+            break;
+        case POR:
+            printf("Porcentaje:\n");
+            printf("Hacamos el porcentaje\n");
+            printf("dime el numero:");
+            scanf("%lf", &s);
+            printf("dime el tanto por ciento:");
+            scanf("%lf", &p);
+            s = s* p/100;
+            printf("el porcentaje es %.2lf\n", s);
+            break;
+        default:
+            printf("del 1 al 6 huevon \n");
+    }
+
+
+
     return EXIT_SUCCESS;
 }
 
