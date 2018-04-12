05f5ae9123dc37a14744223f7029ef989951ac46
sergiosanzllamas <sanzsergio4ab@gmail.com>
2018-04-12 19:24:17 +0200

    jjj

--- contraseña.cpp
+++ contraseña.cpp
@@ -9,19 +9,20 @@ int main(){
 int numero;
 
 
-int intentos = 5;
+int intentos;
 
-for(int intentos = 5; intentos <6; intentos++){
+for(int intentos = 5; intentos >=0; intentos--){
 
 printf("dame el nuemro de lados de un triangulo:");
 scanf("%i", &numero);
 
-if(numero == 3)
+if(numero == 3){
 	printf("has acertado\n");
-
-
-else 
-	printf("llevas %i intentos\n", intentos--);
+intentos = 0;
+}
+else{ 
+	printf("llevas %i intentos\n", intentos);
+}
 }
 
 
